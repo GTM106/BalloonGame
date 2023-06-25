b2bffe78b8ba31ca84ce1e121df1ccd5834b7d04
GTM106 <raito125dragon7@gmail.com>
2023-06-25 14:18:58 +0900

    風船の膨らみにより落下速度と移動速度の変更を実装しました。

--- BalloonGame/Assets/Prototype/Scripts/PlayerController.cs
+++ BalloonGame/Assets/Prototype/Scripts/PlayerController.cs
@@ -1,15 +1,33 @@
 using Cysharp.Threading.Tasks;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(GroundCheck), typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    enum BalloonState
+    {
+        Normal,
+        Expands
+    }
+
     [SerializeField] GameObject balloon;
     [SerializeField] float speed = 5f;
     [SerializeField] InputActionReference move;
     [SerializeField] InputActionReference jump;
+    [SerializeField] GroundCheck groundCheck;
+
+    bool isAnimation = false;
+    private float movementX;
+    private float movementY;
+    private Rigidbody rb;
+    [SerializeField] float jumpPower = 10f;
+    [SerializeField] float multiplier = 3f;
+    [SerializeField] float dashSpeed = 10f;
+
+    Vector2 movementVector;
+
+    BalloonState balloonState = BalloonState.Normal;
 
     Vector3 _offset = Vector3.one / 2f;
 
@@ -24,21 +42,21 @@ public class PlayerController : MonoBehaviour
 
     private void Jump_performed(InputAction.CallbackContext obj)
     {
-        print("jump");
-        rb.AddForce(new(0f, 5f, 0f), ForceMode.Impulse);
+        if (!groundCheck.IsGround(out _)) return;
+        rb.AddForce(new(0f, jumpPower, 0f), ForceMode.Impulse);
     }
 
     private void Action_canceled(InputAction.CallbackContext obj)
     {
         //print("canceled");
-        //movementX = 0f;
-        //movementY = 0f;
+        movementX = 0f;
+        movementY = 0f;
     }
 
     private void Action_performed(InputAction.CallbackContext obj)
     {
         // Moveアクションの入力値を取得
-        Vector2 movementVector = obj.ReadValue<Vector2>();
+        movementVector = obj.ReadValue<Vector2>();
         // print("move axis : " + movementVector);
 
         // x,y軸方向の入力値を変数に代入
@@ -48,39 +66,80 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // 入力値を元に3軸ベクトルを作成
-        Vector3 movement = new(movementX, 0f, movementY);
+        Move();
 
-        // rigidbodyのAddForceを使用してプレイヤーを動かす。
+        //重力の調整
+        AdjustingGravity();
+    }
 
-        rb.AddForce(movement * speed);
-        if (rb.velocity.magnitude > 10f)
+    private void AdjustingGravity()
+    {
+        float multiplierValue = balloonState switch
         {
-            rb.velocity = new Vector3(rb.velocity.x / 1.1f, rb.velocity.y, rb.velocity.z / 1.1f);
-        }
+            BalloonState.Normal => multiplier,
+            BalloonState.Expands => 0.5f,
+            _ => throw new System.NotImplementedException(),
+        };
+
+        rb.AddForce((multiplierValue - 1f) * Physics.gravity, ForceMode.Acceleration);
+    }
+
+    void Move()
+    {
+        //入力値を代入
+        Vector2 axis = movementVector;
+
+        if (axis.magnitude < 0.05f) { return; }
+
+        //Yを無視
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1f, 0f, 1f)).normalized;
+
+        //カメラの向きを元にベクトルの作成
+        Vector3 moveVec = (axis.y * cameraForward + axis.x * Camera.main.transform.right) * speed;
+
+        float balloonSpeed = balloonState switch
+        {
+            //Normal is defalutValue
+            BalloonState.Normal => 1f,
+            BalloonState.Expands => dashSpeed,
+            _ => throw new System.NotImplementedException(),
+        };
+
+        Vector3 velocity = moveVec * balloonSpeed;
+        velocity.y = rb.velocity.y;
+
+        rb.velocity = velocity;
+
+        //if (rb.velocity.magnitude < 10f)
+        //{
+        //    float mag = Mathf.Sqrt(rb.velocity.x * rb.velocity.x + rb.velocity.z * rb.velocity.z);
+        //    float speedMag = 5f - mag;
+        //    rb.AddForce(speedMag * moveVec, ForceMode.Acceleration);
+        //}
     }
 
     void Update()
     {
-        if (Gamepad.current != null && Gamepad.current.leftShoulder.wasPressedThisFrame)
+        if (Gamepad.current != null && Gamepad.current.rightShoulder.wasPressedThisFrame)
         {
-            print("push!!");
+            //print("push!!");
+            balloonState = BalloonState.Expands;
             ScaleAnimation(0.1f, _offset).Forget();
+            return;
         }
 
         if (balloon.transform.localScale.x > 0.5f)
         {
-            float scaleDecrease = 0.001f;
+            float scaleDecrease = 0.2f * Time.deltaTime;
             var scale = balloon.transform.localScale;
             scale.Set(balloon.transform.localScale.x - scaleDecrease, balloon.transform.localScale.y - scaleDecrease, balloon.transform.localScale.z - scaleDecrease);
             balloon.transform.localScale = scale;
         }
+        else
+        {
+            balloonState = BalloonState.Normal;
+        }
     }
-
-    bool isAnimation = false;
-    private float movementX;
-    private float movementY;
-    private Rigidbody rb;
 
     private async UniTask ScaleAnimation(float duration, Vector3 offset)
     {
