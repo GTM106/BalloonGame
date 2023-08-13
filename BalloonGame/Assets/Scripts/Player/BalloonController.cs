330f79991d64d5b7babc3e0b310ebbd55ffb9f37
yuto-0122 <90639451+yuto-0122@users.noreply.github.com>
2023-08-13 16:16:49 +0900

    風船を膨らませる処理を実装しました

---
+++ BalloonGame/Assets/Scripts/Player/BalloonController.cs
@@ -0,0 +1,40 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour
+{
+    [SerializeField] float _scaleAnimationDuration = 0.1f;
+    [SerializeField] Vector3 _scaleOffset = Vector3.one / 2f;
+
+    bool _isAnimation = false;
+    public void Expand()
+    {
+        ScaleAnimation().Forget();
+    }
+
+    private async UniTask ScaleAnimation()
+    {
+        if (_isAnimation) return;
+
+        var token = this.GetCancellationTokenOnDestroy();
+        float time = 0f;
+        Vector3 startVec = transform.localScale;
+
+        while (time < _scaleAnimationDuration)
+        {
+            _isAnimation = true;
+            time += Time.deltaTime;
+            float progress = Mathf.Clamp01(time / _scaleAnimationDuration);
+
+            Vector3 scale = startVec;
+            scale += _scaleOffset * progress;
+            transform.localScale = scale;
+
+            await UniTask.Yield(token);
+        }
+        _isAnimation = false;
+    }
+}
---
+++ BalloonGame/Assets/Scripts/Player/BalloonHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BalloonHandler : MonoBehaviour
+{
+    [SerializeField] InputActionReference _ringPushAction = default!;
+    [SerializeField] BalloonController _balloonController = default!;
+    private void Awake()
+    {
+        _ringPushAction.action.performed += OnRingconPushed;
+    }
+
+    private void OnRingconPushed(InputAction.CallbackContext obj)
+    {
+        _balloonController.Expand();
+    }
+}
