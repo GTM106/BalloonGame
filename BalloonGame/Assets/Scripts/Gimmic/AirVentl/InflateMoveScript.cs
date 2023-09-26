bfe4b186116a4e68e47ec4ba77ca990ae5123cdd
yuto-0122 <90639451+yuto-0122@users.noreply.github.com>
2023-09-26 11:42:48 +0900

    修正点を修正しました。

--- BalloonGame/Assets/Scripts/Gimmic/AirVentl/InflateMoveScript.cs
+++ BalloonGame/Assets/Scripts/Gimmic/AirVentl/InflateMoveScript.cs
@@ -14,24 +14,30 @@ public class InflateMoveScript : AirVentInteractable
     [Header("ギミックが最大移動距離から元の位置に戻るかどうか")]
     [SerializeField] bool arrivalBack = default!;
     [Header("プッシュ時に進む単位")]
-    [SerializeField] float moveSpeed = default!;
+    [SerializeField, Min(0f)] float moveSpeed = default!;
     [Header("ギミックが元の位置に戻る単位")]
     [SerializeField] float returnSpeed = default!;
     [Header("ギミックが元の位置に戻り始めるまでの単位")]
     [SerializeField] float stopTime = default!;
 
-    Vector3 initialPoint = Vector3.zero;
+    Vector3 initialPoint;
 
-    //変数名変えろ
-    bool initjalMove = false;
+    bool hasMovedFromStartPosition = false;
     float rollBackTime = 0f;
-    float minDistance = 0.1f;
+    const float MIN_DISTANCE = 0.1f;
 
     private void Awake()
     {
         initialPoint = transform.position;
     }
 
+    private void Update()
+    {
+        if (rollBackTime >= stopTime)
+        {
+            RollBack();
+        }
+    }
     private void FixedUpdate()
     {
         RoleBackChecker();
@@ -40,38 +46,37 @@ public class InflateMoveScript : AirVentInteractable
     private void RoleBackChecker()
     {
         if (!rollBack) return;
-        if (!initjalMove) return;
+        if (!hasMovedFromStartPosition) return;
 
-        rollBackTime += Time.deltaTime;
-
-        if (rollBackTime >= stopTime)
-        {
-            RollBack();
-        }
+        //戻るまでの時間計測
+        rollBackTime += Time.fixedDeltaTime;
     }
 
     private void RollBack()
     {
+        //初期地点まで移動
         transform.position = Vector3.MoveTowards(transform.position, initialPoint, returnSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, initialPoint) <= 0.1f)
+        if (Vector3.Distance(transform.position, initialPoint) <= MIN_DISTANCE)
         {
-            initjalMove = false;
+            hasMovedFromStartPosition = false;
             rollBackTime = 0f;
         }
     }
 
     public override void Interact()
     {
+        //指定地点へ移動
         transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, moveSpeed * Time.deltaTime);
 
-        initjalMove = true;
+        hasMovedFromStartPosition = true;
         rollBackTime = 0f;
 
-        if (Vector3.Distance(transform.position, targetPoint.position) <= minDistance)
+        if (Vector3.Distance(transform.position, targetPoint.position) <= MIN_DISTANCE)
         {
             if (!arrivalBack)
             {
+                //ギミックが最大移動距離に到達した場合RollBackをオフにする
                 rollBack = false;
             }
         }
