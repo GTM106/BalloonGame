f9aa87f630a2da8d7c9583884c7c453626bb3906
GTM106 <raito125dragon7@gmail.com>
2023-12-17 05:35:11 +0900

    担当分のUIの組み込みを行いました
    詳細な位置などは要修正です。

--- BalloonGame/Assets/Scripts/TimeLimit/TimeLimitView.cs
+++ BalloonGame/Assets/Scripts/TimeLimit/TimeLimitView.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeLimitView : MonoBehaviour
 {
     [SerializeField, Required] Canvas _timeLimitCanvas = default!;
-    [SerializeField, Required] TMP_Text _text;
+    [SerializeField, Required] Image _timeLimitBar = default!;
+    [SerializeField, Required] Image _timeLimitClockHand = default!;
+
+    const float positionOffset = -1850f;
+    const float rotationOffset = 360f;
+
+    Vector3 _firstTimeLimitBarPos;
+
+    private void Awake()
+    {
+        _firstTimeLimitBarPos = _timeLimitBar.transform.position;
+    }
 
     public void Enable()
     {
@@ -18,8 +30,10 @@ public class TimeLimitView : MonoBehaviour
         _timeLimitCanvas.enabled = false;
     }
 
-    public void UpdateText(string text)
+    public void UpdateView(float progress)
     {
-        _text.text = text;
+        _timeLimitBar.fillAmount = Mathf.Clamp01(progress);
+        _timeLimitBar.transform.position = _firstTimeLimitBarPos + positionOffset * (1f - progress) * Vector3.right;
+        _timeLimitClockHand.transform.rotation = Quaternion.Euler(0f, 0f, rotationOffset * progress);
     }
 }
